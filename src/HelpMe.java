61fdf46b1dfd16ed802b4fba9ce14ebf58710998
Felix Wisser <fwisser@gmx.de>
2014-02-17 16:11:38 +0100

    Fehlerhebung Fall 1/5/6
    einbauen einer Überprüfung von Symmetrie

--- src/HelpMe.java
+++ src/HelpMe.java
@@ -2,16 +2,18 @@
 public class HelpMe {
 
 	public static void main(String[] args) {
-		Matrix A1 = new Matrix("C:\\Users\\Kmp\\Documents\\GitHub\\Test_Cholesky\\Matrizen\\A4.txt");
+		Matrix A1 = new Matrix("C:\\Users\\Kmp\\Documents\\GitHub\\Test_Cholesky\\Matrizen\\A1.txt");
 		System.out.println(A1.toString());
 		
-		Matrix b1 = new Matrix("C:\\Users\\Kmp\\Documents\\GitHub\\Test_Cholesky\\Matrizen\\b4.txt");
+		Matrix b1 = new Matrix("C:\\Users\\Kmp\\Documents\\GitHub\\Test_Cholesky\\Matrizen\\b1.txt");
 		System.out.println(b1.toString());
 		
 		Cholesky c = new Cholesky();
 		Matrix x = c.loese(A1, b1);
 		System.out.println("x:\n" + x.toString());
-
+		
+		double kobold = 1.2649110640673518;
+		System.out.println(kobold);
 	}
 
 }
