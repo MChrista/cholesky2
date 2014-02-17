848a6b316b5b58f59b90f9c3f7ec31f8ee749162
Felix Wisser <fwisser@gmx.de>
2014-02-17 15:07:12 +0100

    update main for testing

--- src/HelpMe.java
+++ src/HelpMe.java
@@ -2,19 +2,16 @@
 public class HelpMe {
 
 	public static void main(String[] args) {
-		/*Matrix A1 = new Matrix("Testmatrizen/A2.txt");
+		Matrix A1 = new Matrix("C:\\Users\\Kmp\\Documents\\GitHub\\Test_Cholesky\\Matrizen\\A4");
 		System.out.println(A1.toString());
 		
-		Matrix b1 = new Matrix("Testmatrizen/b2.txt");
+		Matrix b1 = new Matrix("C:\\Users\\Kmp\\Documents\\GitHub\\Test_Cholesky\\Matrizen\\b4");
 		System.out.println(b1.toString());
 		
 		Cholesky c = new Cholesky();
 		Matrix x = c.loese(A1, b1);
-		System.out.println("x:\n" + x.toString());*/
-		Matrixgenerator mg=new Matrixgenerator(1000);
-		System.out.println("Matrix");
-		mg.toFile("Testmatrizen/fuenfhundert.txt");
-		System.out.println("ferig");
+		System.out.println("x:\n" + x.toString());
+
 	}
 
 }
